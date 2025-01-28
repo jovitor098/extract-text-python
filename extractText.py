6f351c4c209c63da5b78066db897b0207b593c5c
jovitor <joaovitorguilherme788@gmail.com>
2025-01-28 11:26:59 -0300

    add raise exception

--- extractText.py
+++ extractText.py
@@ -1,9 +1,13 @@
 from PIL import Image
 from io import BytesIO
 import pytesseract
+from exception.loadImage import LoadImageException
 
 async def extract_text(file):
-    content = await file.read()
-    img = Image.open(BytesIO(content))
-    text = pytesseract.image_to_string(img)
-    return text
+    try:
+        content = await file.read()
+        img = Image.open(BytesIO(content))
+        text = pytesseract.image_to_string(img)
+        return text
+    except:
+        raise LoadImageException("Erro while load image")
