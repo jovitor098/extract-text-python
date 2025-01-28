a24312cf81d999aca60dc3ac86934b72b17c25ee
jovitor <joaovitorguilherme788@gmail.com>
2025-01-28 11:34:42 -0300

    feature: add exception handler to exceptions

--- main.py
+++ main.py
@@ -20,6 +20,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.exception_handler(MissingImageException)
+async def missing_image_exception_handler(request: Request, exc: MissingImageException):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST, 
+        content={"error": f"Check if you upload image: {exc.msg}"})
+
+@app.exception_handler(LoadImageException)
+async def missing_image_exception_handler(request: Request, exc: LoadImageException):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST, 
+        content={"error": f"Check if the file is a image: {exc.msg}"})
+
+
 @app.post("/extract")
 async def extract(request: Request):
     form = await request.form()
